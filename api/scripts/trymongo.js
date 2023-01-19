aa3e954152ae77bd73880a8cf6fe46ac46e963fd
Yann Youbi <youbi.yann@gmail.com>
2023-01-19 16:28:45 -0500

    7

---
+++ api/scripts/trymongo.js
@@ -0,0 +1,68 @@
+const { MongoClient } = require('mongodb');
+
+const url = "mongodb+srv://yann-ybi:mypassword@issuetracker.kewi82v.mongodb.net/?retryWrites=true&w=majority";
+
+function testWithCallbacks(callback) {
+    console.log('\n--- testWithCallbacks ---');
+    const client = new MongoClient(url, { useNewUrlParser: true });
+    client.connect(function(err, client) {
+      if (err) {
+        callback(err);
+        return;
+      }
+      console.log('Connected to MongoDB');
+  
+      const db = client.db();
+      const collection = db.collection('employees');
+  
+      const employee = { id: 1, name: 'A. Callback', age: 23 };
+      collection.insertOne(employee, function(err, result) {
+        if (err) {
+          client.close();
+          callback(err);
+          return;
+        }
+        console.log('Result of insert:\n', result.insertedId);
+        collection.find({ _id: result.insertedId})
+          .toArray(function(err, docs) {
+          if (err) {
+            client.close();
+            callback(err);
+            return;
+          }
+          console.log('Result of find:\n', docs);
+          client.close();
+          callback(err);
+        });
+      });
+    });
+  }
+  
+async function testWithAsync() {
+    console.log('\n--- testWithAsync ---');
+    const client = new Mongoclient(url, { useNewUrlParser: true });
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+        const db = client.db();
+        const collection = db.collection('employees');
+
+        const employee = { id: 2, name: 'B. Async', age: 16 };
+        const result = await collection.insertOne(employee);
+        console.log('Result of insert:\n', result.insertedId);
+
+        const docs = await collection.find({ _id: result.insertedID }).toArray();
+        console.log('Result of find: \n', docs);
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.close();
+    }
+}
+  
+testWithCallbacks(function(err) {
+if (err) {
+    console.log(err);
+}
+testWithAsync();
+});
---
+++ api/server.js
@@ -0,0 +1,113 @@
+const fs = require('fs');
+const express = require('express');
+const { ApolloServer, UserInputError } = require('apollo-server-express');
+const { GraphQLScalarType, parseValue } = require('graphql');
+const { Kind } = require('graphql/language');
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb+srv://yann-ybi:mypassword@IssueTracker-issuetracker.kewi82v.mongodb.net/issuetracker?retryWrites=true';
+
+let db;
+
+let aboutMessage = "Issue tracker API v1.0";
+
+const GraphQLDate = new GraphQLScalarType({
+    name: 'GraphQLDate',
+    description: 'A Date() type in GraphQL as a scalar',
+    serialize(value) {
+        return value.toISOString();
+    },
+    parseValue(value) {
+        const dateValue = new Date(value);
+        return isNaN(dateValue) ? undefined : dateValue;
+    },
+    parseLiteral(ast) {
+        if (ast.kind == Kind.STRING) {
+            const value = new Date(ast.value);
+            return isNaN(value) ? undefined : value;
+        }
+    }
+})
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage, issueList
+    },
+    Mutation: {
+        setAboutMessage,
+        issueAdd,
+    },
+    GraphQLDate
+}
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message
+}
+
+async function issueList() {
+    const issues = await db.collection('issues').find({}).toArray();
+    return issues
+}
+
+async function getNextSequence(name) {
+    const result = await db.collection('counters').findOneAndUpdate(
+        { _id: name },
+        { $inc: { current: 1 } },
+        { returnOriginal: false },
+    );
+    return result.value.current;
+}
+
+function issueValidate(issue) {
+    const errors = [];
+    if (issue.title.length < 3) {
+        errors.push('Filed "title" must be at least 3 characters long.');
+    }
+    if (issue.status === 'Assigned' && !issue.owner) {
+        errors.push('Fieled "owber" is required when status is "Assigned"');
+    }
+    if (errors.length > 0) {
+        throw new UserInputError('Invalid input(s)', { errors });
+    }
+}
+
+async function issueAdd(_, { issue }) {
+    issueValidate(issue);
+    issue.created = new Date();
+    issue.id = await getNextSequence('issues');
+
+    const result = await db.collection('issues').insertOne(issue);
+    const savedIssue = await db.collection('issues').findOne({ _id: result.insertedID});
+    return savedIssue;
+}
+
+async function connectToDb() {
+    const client = new MongoClient(url, { useNewUrlParser: true });
+    await client.connect();
+    console.log('Connected to MongoDB at', url);
+    db = client.db()
+}
+
+const server = new ApolloServer({
+    typeDefs: fs.readFileSync('schema.graphql', 'utf-8'),
+    resolvers,
+    formatError: error => {
+        console.log(error);
+        return error;
+    }
+})
+
+const app = express();
+
+server.applyMiddleware({ app, path: '/graphql' });
+
+(async function () {
+    try { 
+        await connectToDb();
+        app.listen(3000, function () {
+            console.log('API server started on port 3000');
+        });
+    } catch (err) {
+        console.log('ERROR', err);
+    }
+})();
